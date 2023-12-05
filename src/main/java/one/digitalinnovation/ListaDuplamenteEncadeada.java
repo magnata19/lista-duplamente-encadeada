ba0addf7eb38a1ba11633ab4b9579f93b8609ebd
Davidson <dehpacifico@gmail.com>
2023-12-05 20:25:25 -0300

    implementando método add

--- src/main/java/one/digitalinnovation/ListaDuplamenteEncadeada.java
+++ src/main/java/one/digitalinnovation/ListaDuplamenteEncadeada.java
@@ -17,6 +17,22 @@ public class ListaDuplamenteEncadeada<T> {
         return this.getNo(index).getConteudo();
     }
 
+    public void add(T elemento) {
+        NoDuplo<T> novoNo = new NoDuplo<>(elemento);
+        novoNo.setNoProximo(null);
+        novoNo.setNoPrevio(ultimoNo);
+
+        if(primeiroNo == null) {
+            primeiroNo = novoNo;
+        }
+
+        if(ultimoNo != null) {
+            ultimoNo.setNoProximo(novoNo);
+        }
+        ultimoNo = novoNo;
+        tamanhoLista++;
+    }
+
     private NoDuplo<T> getNo(int index){
         NoDuplo<T> noAuxiliar = primeiroNo;
 
@@ -27,6 +43,6 @@ public class ListaDuplamenteEncadeada<T> {
     }
 
     public int size() {
-        return tamanhoLista;
+        return this.tamanhoLista;
     }
 }
